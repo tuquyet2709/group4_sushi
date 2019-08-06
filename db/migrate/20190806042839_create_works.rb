7bf8d0e04b9b8bd76275d5393810a1981b262ce5
anhtuhsp <anhtuhspk61@gmail.com>
2019-08-06 15:32:42 +0700

    stash

--- db/migrate/20190806042839_create_works.rb
+++ db/migrate/20190806042839_create_works.rb
@@ -1,6 +1,7 @@
 class CreateWorks < ActiveRecord::Migration[5.1]
   def change
     create_table :works do |t|
+      t.integer :company_id
       t.string :title
       t.datetime :dateline
       t.integer :salary
@@ -8,6 +9,7 @@ class CreateWorks < ActiveRecord::Migration[5.1]
       t.string :work_location
       t.string :content
       t.boolean :status , default: false
+      t.boolean :process_status, default: false
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,47 @@
 # Admin.create!(email: "admin@gmail.com",
 #              password:              "123456",
 #              password_confirmation: "123456")
+program_languages = ["C", "C++", "Java", "Ruby", "Python", "Javascript",
+                     "Nodejs", "R", "PHP"]
+
 10.times do |n|
   email = "student-#{n+1}@gmail.com"
   name = "student-#{n+1}"
   password = "123456"
+  phone = (0...9).map { rand(10)}.join
+  age = "1#{n}"
+  school = "school-#{n+1}"
+  program_language = program_languages.sample(1 + rand(program_languages.count)).join(", ")
+  pr_content = "I'm a good student, hire me!"
+  avg_score = rand(5) + 1
   Student.create!(email: email,
-                name: name,
-               password:              password,
-               password_confirmation: password
-              )
+                  name: name,
+                  password:              password,
+                  password_confirmation: password,
+                  phone: phone,
+                  age: age,
+                  program_language: program_language,
+                  pr_content: pr_content,
+                  avg_score: avg_score
+                )
 end
 
+addresses = ["Ha noi", "Ho Chi Minh", "Tokyo", "Saitama", "Osaka"]
 10.times do |n|
   email = "company-#{n+1}@gmail.com"
   name = "company-#{n+1}"
   password = "123456"
+  address = addresses.sample
+  pr_content = "Our company have mission to bring future to the world"
+  phone = (0...9).map { rand(10)}.join
   Company.create!(email: email,
                name: name,
                password:              password,
-               password_confirmation: password
+               password_confirmation: password,
+               address: address,
+               pr_content: pr_content,
+               phone: phone
   )
 end
+
+15.times do |n|
