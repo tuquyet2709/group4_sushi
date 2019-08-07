77dfd004f35932da1b7a334ec2a4289709e9c7bd
tuquyet2709 <goldenlightning1252@gmail.com>
2019-08-08 01:01:58 +0700

    role login

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,15 +2,33 @@ class SessionController < ApplicationController
   def new
   end
 
+  # def create
+  #   user = Company.find_by(email: params[:session][:email].downcase)
+  #   if user.nil?
+  #     user = Student.find_by(email: params[:session][:email].downcase)
+  #   end
+  #
+  #   if user && user.authenticate(params[:session][:password])
+  #     log_in user
+  #     redirect_to user
+  #   else
+  #     flash.now[:danger] = "Invalid email or password!"
+  #     render :new
+  #   end
+  # end
+
   def create
-    user = Company.find_by(email: params[:session][:email].downcase)
-    if user.nil?
-      user = Student.find_by(email: params[:session][:email].downcase) 
+    company = Company.find_by(email: params[:session][:email].downcase)
+    if company.nil?
+      student = Student.find_by(email: params[:session][:email].downcase)
     end
 
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+    if company && company.authenticate(params[:session][:password])
+      log_in_company company
+      redirect_to company
+    elsif student && student.authenticate(params[:session][:password])
+      log_in_student student
+      redirect_to student
     else
       flash.now[:danger] = "Invalid email or password!"
       render :new
@@ -18,7 +36,8 @@ class SessionController < ApplicationController
   end
 
   def destroy
-    log_out if logged_in?
+    log_out_student if logged_in_student?
+    log_out_company if logged_in_company?
     redirect_to root_url
   end
 
--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,32 +1,63 @@
 module SessionHelper
-  def log_in(user)
-    session[:user_id] = user.id
+  # def log_in(user)
+  #   session[:user_id] = user.id
+  # end
+  #
+  # def current_user
+  #   if session[:user_id]
+  #     if Student.find_by(id: session[:user_id])
+  #       @current_user ||= Student.find_by(id: session[:user_id])
+  #     else
+  #       @current_user ||= Company.find_by(id: session[:user_id])
+  #     end
+  #   end
+  # end
+
+  def log_in_student(student)
+    session[:student_id] = student.id
   end
 
-  def current_user
-    if session[:user_id]
-      if Student.find_by(id: session[:user_id])
-        @current_user ||= Student.find_by(id: session[:user_id])
-      else
-        @current_user ||= Company.find_by(id: session[:user_id])
-      end
+  def log_in_company(company)
+    session[:company_id] = company.id
+  end
+
+  # def current_user
+  #   if session[:user_id]
+  #     if Student.find_by(id: session[:user_id])
+  #       @current_user ||= Student.find_by(id: session[:user_id])
+  #     else
+  #       @current_user ||= Company.find_by(id: session[:user_id])
+  #     end
+  #   end
+  # end
+
+  def current_student
+    if session[:student_id]
+      @current_student ||= Student.find_by(id: session[:student_id])
     end
   end
 
-  def logged_in?
-    current_user.present?
+  def current_company
+    if session[:company_id]
+      @current_company ||= Company.find_by(id: session[:company_id])
+    end
   end
-  #
-  # def is_student?
-  #   current_user == "Student"? 1 : 0
-  # end
-  #
-  # def is_company?
-  #   current_user == "Company"? 1 : 0
-  # end
 
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
+  def logged_in_student?
+    current_student.present?
+  end
+
+  def logged_in_company?
+    current_company.present?
+  end
+
+  def log_out_student
+    session.delete(:student_id)
+    @current_student = nil
+  end
+
+  def log_out_company
+    session.delete(:company_id)
+    @current_company = nil
   end
 end
