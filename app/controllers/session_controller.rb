30593fb683add9deb872da271ef1c0f5f2fa5a80
tuquyet2709 <goldenlightning1252@gmail.com>
2019-08-08 02:09:21 +0700

    fix my page company

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -10,10 +10,10 @@ class SessionController < ApplicationController
 
     if company && company.authenticate(params[:session][:password])
       log_in_company company
-      redirect_to company
+      redirect_to root_path
     elsif student && student.authenticate(params[:session][:password])
       log_in_student student
-      redirect_to student
+      redirect_to root_path
     else
       flash.now[:danger] = "Invalid email or password!"
       render :new
